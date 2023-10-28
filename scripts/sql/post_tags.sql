413b672437b0a35c66a13b34a58ff5ad8aa344b2
Darkzarich <darkzarichv2@gmail.com>
2023-10-28 18:23:28 +0300

    fix post_tags.sql typo

--- scripts/sql/post_tags.sql
+++ scripts/sql/post_tags.sql
@@ -1,8 +1,8 @@
-CREATE TABLE IF NOT EXISTS post_tags {
+CREATE TABLE IF NOT EXISTS post_tags (
   post_id INTEGER NOT NULL REFERENCES posts(id),
   tag_id INTEGER NOT NULL REFERENCES tags(id),
   PRIMARY KEY(post_id, tag_id)
-}
+);
 
 INSERT INTO post_tags (post_id, tag_id) 
 VALUES 
